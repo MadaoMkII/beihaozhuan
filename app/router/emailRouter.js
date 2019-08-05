983f9b1af7c75d934fca1bc2d45ad8ef963cce6e
Shaunli <1702050003@svuca.edu>
2019-08-05 15:04:43 +0800

    sec

--- app/router/emailRouter.js
+++ app/router/emailRouter.js
@@ -2,4 +2,6 @@ module.exports = app => {
     const {router, controller} = app;
     router.get('/mail', controller.emailController.sendEmail);
     router.get('/sms', controller.smsController.sendSmsMessage);
+    router.post('/sendVerifySmsMessage', controller.smsController.sendVerifySmsMessage);
+    router.post('/sendVerifySmsMessage', controller.smsController.sendVerifySmsMessage);
 };
--- app/router/userRouter.js
+++ app/router/userRouter.js
@@ -1,9 +1,10 @@
 module.exports = app => {
     const {router, controller} = app;
+    router.post('/user/updateInfo', controller[`userAccount`].updateUserInfo);
     router.post('/user/register', controller[`authController`].register);
     router.post('/user/login', controller[`authController`].login);
     router.get('/user/logout', controller[`authController`].logout);
     router.get('/index', controller[`home`].index);
-    router.get('/tryuser', controller[`userAccount`].getUserInfo);
+    router.get('/getAll', controller[`userAccount`].getAll);
     router.get('/user/getInfo', app.middleware.authenticatedMiddleware(`User`), controller['userAccount'].getUserInfo);
 };
