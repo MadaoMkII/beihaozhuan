713f5647b525b11705cbc6294164cdc23767698c
shaunli <shaunli319@gmail.com>
2019-09-10 10:36:55 +0800

    develop

--- app/controller/callBackController.js
+++ app/controller/callBackController.js
@@ -4,6 +4,7 @@ const {Controller} = require('egg');
 
 class callBackController extends Controller {
     async getCallBackInfo(ctx) {
+        console.log(`we are here`)
         console.log(ctx.request.query)
         this.success();
     }
