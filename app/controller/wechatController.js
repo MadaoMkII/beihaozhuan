fc1b3f50cc7bd5c37e7440e7a47a14804b15a172
shaunli <shaunli319@gmail.com>
2019-09-19 11:39:58 +0800

    develop

--- app/controller/wechatController.js
+++ app/controller/wechatController.js
@@ -4,14 +4,36 @@ let url = require("url");
 
 class wechatController extends baseController {
 
+    async pre_login(OPENID) {
+
+        await this.ctx.service.userservice.getUser({OPENID: OPENID});
+    };
+
+
     requestMethod(JSONObject, method, url) {
-        return new Promise((resolve, reject) => {
-            request({
+        let requestObj = {};
+        if (method === `GET`) {
+            const myURL = new URL(url);
+            Object.keys(JSONObject).forEach((key) => {
+                myURL.searchParams.append(key, JSONObject[key]);
+            });
+            requestObj = {
+                url: myURL.href,
+                method: method,
+                json: true,   // <--Very important!!!
+            }
+        }
+        if (method === `POST`) {
+            requestObj = {
                 url: url,
                 method: method,
                 json: true,   // <--Very important!!!
                 body: JSONObject
-            }, (error, response, body) => {
+            }
+        }
+        return new Promise((resolve, reject) => {
+            console.log(requestObj)
+            request(requestObj, (error, response, body) => {
                 if (error) {
                     reject(error)
                 } else {
@@ -25,7 +47,15 @@ class wechatController extends baseController {
         console.log(ctx.request)
         const returnUrl = ctx.request.url;
         let urlQuery = url.parse(returnUrl, true).query;
+        const {code, state} = urlQuery;
+        if (ctx.help.isEmpty(code) || ctx.help.isEmpty(state)) {
+            ctx.throw(`空值警告`)
+        }
+        let requestObj_1 = {
+            appid:ctx.app.se
 
+        };
+        this.requestMethod({}, `GET`, `https://api.weixin.qq.com/sns/oauth2/access_token`);
 
         switch (urlQuery) {
             case `login`:
