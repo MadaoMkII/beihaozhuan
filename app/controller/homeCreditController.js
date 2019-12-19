36c7c644bd5d8502da09328f8440b35d0b3c31c0
shaunli <shaunli319@gmail.com>
2019-12-19 17:12:21 +0800

    爆炸了可不赖我

--- app/controller/homeCreditController.js
+++ app/controller/homeCreditController.js
@@ -57,7 +57,7 @@ class homeCreditController extends baseController {
 
   async checkProofsStatus(ctx) {
     try {
-      const result = await ctx.model.DoubleDec.findOne({ userUUid: ctx.user.uuid }, {}, { sort: { created_at: -1 } });
+      const result = await ctx.model.DoubleDec.findOne({ userUUid: ctx.user.uuid, loginPicUrl: { $ne: null } }, {}, { sort: { created_at: -1 } });
       if (!ctx.helper.isEmpty(result)) {
         return this.success({ status: result.status });
       }
